d1104e685921c8384c5852334c3a0f72b78de012
Pouryaakbari <Pouryaakbari20@gmail.com>
2025-11-04 12:08:08 +0330

    part#13 section test alpinjs

--- assets/js/users.js
+++ assets/js/users.js
@@ -15,39 +15,39 @@ document.addEventListener("DOMContentLoaded", function () {
       e.target.classList.add("dis-none");
     });
 
-  setTimeout(function(){
-    console.log('start----');
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((json) => {
-      console.log('end');
-      console.log(json)
-      let html = ""
-      for(const user of json){
-        html = html + ` <tr>
-                          <td>${user.id}</td>
-                          <td>${user.name}</td>
-                          <td>${user.username}</td>
-                            <td>${user.email}</td>
-                            <td>${user.address.city}</td>
-                            <td>
-                              <i class="material-icons red-text m-l-5 m-r-5 hov-pointer"
-                                >delete_forever</i
-                              >
-                              <i
-                                class="material-icons orange-text darken-3 m-l-5 m-r-5 hov-pointer"
-                                >edit</i
-                              >
-                            </td>
-                        </tr>`
-      }
-      document.querySelector('.desktop-tabel-tbody').innerHTML = html
+  // setTimeout(function(){
+  //   console.log('start----');
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //   .then((response) => response.json())
+  //   .then((json) => {
+  //     console.log('end');
+  //     console.log(json)
+  //     let html = ""
+  //     for(const user of json){
+  //       html = html + ` <tr>
+  //                         <td>${user.id}</td>
+  //                         <td>${user.name}</td>
+  //                         <td>${user.username}</td>
+  //                           <td>${user.email}</td>
+  //                           <td>${user.address.city}</td>
+  //                           <td>
+  //                             <i class="material-icons red-text m-l-5 m-r-5 hov-pointer"
+  //                               >delete_forever</i
+  //                             >
+  //                             <i
+  //                               class="material-icons orange-text darken-3 m-l-5 m-r-5 hov-pointer"
+  //                               >edit</i
+  //                             >
+  //                           </td>
+  //                       </tr>`
+  //     }
+  //     document.querySelector('.desktop-tabel-tbody').innerHTML = html
 
 
             
 
 
 
-    });
-  },5000)  
+  //   });
+  // },5000)  
 });
