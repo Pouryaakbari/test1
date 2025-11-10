3ea2880faef8642f138b1ef9f0231f08230fde55
Pouryaakbari <Pouryaakbari20@gmail.com>
2025-11-10 12:02:00 +0330

    part#14 axios.js

---
+++ assets/js/alpainejscontollers/usercontollers.js
@@ -0,0 +1,14 @@
+ document.addEventListener('alpine:init', () => {
+        Alpine.data('userData', () => ({
+           users:[],
+           getUsers(){
+            axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+                this.users = res.data
+                console.log(res);
+
+
+            }
+        )}
+           
+    }))
+    })
