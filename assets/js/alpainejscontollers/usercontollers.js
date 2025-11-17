c22f735da460c793b742dd598fa031fc19b31048
Pouryaakbari <Pouryaakbari20@gmail.com>
2025-11-17 11:45:41 +0330

    part#15 components preloader (is loading)

--- assets/js/alpainejscontollers/usercontollers.js
+++ assets/js/alpainejscontollers/usercontollers.js
@@ -1,9 +1,12 @@
  document.addEventListener('alpine:init', () => {
         Alpine.data('userData', () => ({
            users:[],
+           isloading:false,
            getUsers(){
+            this.isloading = true
             axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
                 this.users = res.data
+                this.isloading = false
                 console.log(res);
 
 
