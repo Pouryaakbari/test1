20a808f7dbd8ad9bb9d017f06ceeb814e3d461e6
Pouryaakbari <Pouryaakbari20@gmail.com>
2025-11-18 09:30:01 +0330

    part#16 button add user (x-bind/x-on)

--- assets/js/alpainejscontollers/usercontollers.js
+++ assets/js/alpainejscontollers/usercontollers.js
@@ -2,16 +2,20 @@
         Alpine.data('userData', () => ({
            users:[],
            isloading:false,
+           showaddmodal:false,
            getUsers(){
             this.isloading = true
             axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
                 this.users = res.data
-                this.isloading = false
                 console.log(res);
 
 
-            }
-        )}
+            }).catch(error=>{
+                console.log(error.message);
+            }).finally(()=>{
+                this.isloading = false
+            })
+        }
            
     }))
     })
