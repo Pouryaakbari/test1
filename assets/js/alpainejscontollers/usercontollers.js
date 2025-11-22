c6b647b93c661c33da1ae5f80ceaf940369ba1a4
Pouryaakbari <Pouryaakbari20@gmail.com>
2025-11-22 13:26:41 +0330

    part#17 pageusers (pagination)

--- assets/js/alpainejscontollers/usercontollers.js
+++ assets/js/alpainejscontollers/usercontollers.js
@@ -1,12 +1,17 @@
  document.addEventListener('alpine:init', () => {
         Alpine.data('userData', () => ({
            users:[],
+           pageusers : [],
            isloading:false,
            showaddmodal:false,
+           pagecount : 1 ,
+           itemcount : 4 ,
+           cuurnpage : 1 ,
            getUsers(){
             this.isloading = true
             axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
                 this.users = res.data
+                this.pagination()
                 console.log(res);
 
 
@@ -15,6 +20,12 @@
             }).finally(()=>{
                 this.isloading = false
             })
+        },
+        pagination(){
+            this.pagecount = Math.ceil( this.users.length / this.itemcount)
+            let start = (this.cuurnpage * this.itemcount) - this.itemcount
+            let end = this.cuurnpage * this.itemcount
+            this.pageusers=this.users.slice(start,end)
         }
            
     }))
